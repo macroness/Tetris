71e717dd30684d9223d9577b286b5613dd06d208
김현우 <macroness@synapsoft.co.kr>
2019-02-24 20:06:50 +0900

    테트리스가 돌아갈 루프생성

--- main.py
+++ main.py
@@ -5,13 +5,39 @@ pygame.init()
 screen_w = 700
 screen_h = 900
 
-win = pygame.display.set_mode((screen_w, screen_h))
+def gameStart(surface):
+
+    run = True
+
+    while run:
+        font = pygame.font.SysFont("Arial", 20)
+        # 둥근 모서리로 흰색 글자 그리기
+        textBox = font.render("Playing...", 1, (255, 255, 255))
+        surface.blit(textBox, (0, 0))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+def menu(surface):
+    run = True
+
+    while run:
+        surface.fill((0,0,0)) # 메뉴 창 검정색 바탕
+
+        font = pygame.font.SysFont("Arial", 20)
+        # 둥근 모서리로 흰색 글자 그리기
+        textBox = font.render("Press Any Key ...", 1, (255, 255, 255))
+        surface.blit(textBox, (350, 450))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                gameStart(surface)
+
+
+surface = pygame.display.set_mode((screen_w, screen_h))
 pygame.display.set_caption('KiMiCa\'s Tetris')
 
-run = True
-
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        pygame.display.update()
+menu(surface)
