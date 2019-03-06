475f6a4b0d07c6dcce163e609bc79a74071493d0
macroness <macroness@gmail.com>
2019-03-07 00:33:09 +0900

    line 삭제 로직 추가.

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ screen_h = 900
 grid_w = 10 * 30
 grid_h = 20 * 30
 
+grid_col = 12
+grid_row = 22
+
 # grid 시작 점 (x,y) 좌표 (왼쪽 위 꼭지점)
 grid_x = (screen_w - grid_w) // 2
 grid_y = (screen_h - grid_h) // 2
@@ -36,7 +39,7 @@ class Conflict(enum.Enum):
     BLOCK = 5
 
 def getRandomBlock():
-    return Block(3, -3, random.randrange(0, 7))
+    return Block(-3, 3, random.randrange(0, 7))
 
 # 블럭 영역에서 실제로 블럭이 그려질 좌표 list 반환
 def getValidPositions(block):
@@ -44,46 +47,46 @@ def getValidPositions(block):
     for i in range(5):
         for j in range(5):
             if block.block[block.rotation][i][j] == 'X':
-                positions.append((j + block.x, i + block.y))
+                positions.append((i + block.x, j + block.y))
     return positions
 
 def drawBlock(grid, positions, color):
     for pos in positions:
-        if pos[1] >= 0:
+        if pos[0] >= 0:
             grid[pos[0]][pos[1]] = color
 
 def drawGrid(surface, grid):
     # 회색 배경
     pygame.draw.rect(surface, (160, 160, 160), (grid_x, grid_y, grid_w + 8 * inline_w, grid_h + 18 * inline_w))
 
-    for i in range(0,len(grid) - 2):
-        x = grid_x + (i * (inline_w + blockSize))
-        for j in range(0, len(grid[i]) - 2):
-            y = grid_y + (j * (inline_w + blockSize))
+    for i in range(0, grid_row - 2):
+        y = grid_y + (i * (inline_w + blockSize))
+        for j in range(0, grid_col - 2):
+            x = grid_x + (j * (inline_w + blockSize))
             pygame.draw.rect(surface, grid[i+1][j+1], (x, y, blockSize, blockSize))
 
     pygame.draw.rect(surface, (180, 0, 180), (grid_x - 3, grid_y - 3, grid_w + (8 * inline_w) + 6, grid_h + (18 * inline_w) + 6), outline_w)
 
 # grid는 한 칸에 표현할 색을 갖고있음.
 def createGrid():
-    grid = [[(0,0,0) for _ in range(22)] for _ in range(12)]
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    grid = [[(0,0,0) for _ in range(grid_col)] for _ in range(grid_row)]
+    for i in range(grid_row):
+        for j in range(grid_col):
             # TOP wall
-            if j == 0:
+            if i == 0:
                 grid[i][j] = (1,1,1)
 
             # BOTTOM wall
-            if j == len(grid[i]) - 1:
+            if i == grid_row - 1:
                 grid[i][j] = (2,2,2)
 
             # LEFT, RIGHT를 뒤에 배치시켜서 각꼭지점의 경우 LEFT와 RIGHT로 구분 되도록 했음. ex) grid[0][0]의 경우 LEFT와 TOP의 영역을 공유하는데 LEFT로 설정되도록함.
             # LEFT wall
-            if i == 0:
+            if j == 0:
                 grid[i][j] = (3,3,3)
 
             # RIGHT wall
-            if i == len(grid) - 1:
+            if j == grid_col - 1:
                 grid[i][j] = (4,4,4)
 
     return grid
@@ -108,15 +111,35 @@ def checkConflict(grid, block, ignoreList = []):
 
     return Conflict.NONE
 
+def fillDeletedLine(grid, x):
+    for i in range(x, 1, -1):
+        grid[i] = copy.deepcopy(grid[i - 1])
+    grid[0] = [(0,0,0) for _ in range(grid_col)]
+
+def deleteLine(grid):
+    for i in range(1, 21):
+        delLine = False
+        for j in range(1, 11):
+            if grid[i][j] != (0,0,0):
+                delLine = True
+            else:
+                delLine = False
+                break
+
+        if delLine:
+            for j in range(1,11):
+                grid[i][j] = (0,0,0)
+            fillDeletedLine(grid, i)
+
 def getDroppedDistance(grid, block):
     tmpBlock = copy.deepcopy(block)
     while True:
-        tmpBlock.y += 1
+        tmpBlock.x += 1
         if checkConflict(grid, tmpBlock, [Conflict.TOP]) != Conflict.NONE:
-            tmpBlock.y -= 1
+            tmpBlock.x -= 1
             break
 
-    return tmpBlock.y - block.y
+    return tmpBlock.x - block.x
 
 def updateScreen(surface, grid):
     surface.fill((0,0,0))
@@ -145,21 +168,21 @@ def gameStart(surface):
                     break
                 elif event.key == pygame.K_SPACE:
                     distance = getDroppedDistance(copiedGrid, currentBlock)
-                    currentBlock.y += distance
+                    currentBlock.x += distance
                     droppedBlock = True
                 elif event.key == pygame.K_DOWN:
-                    currentBlock.y += 1
+                    currentBlock.x += 1
                     if checkConflict(grid, currentBlock, [Conflict.TOP]) != Conflict.NONE:
-                        currentBlock.y -= 1
+                        currentBlock.x -= 1
                         droppedBlock = True
                 elif event.key == pygame.K_LEFT:
-                    currentBlock.x -= 1
+                    currentBlock.y -= 1
                     if checkConflict(grid, currentBlock, [Conflict.BOTTOM, Conflict.TOP]) != Conflict.NONE:
-                        currentBlock.x += 1
+                        currentBlock.y += 1
                 elif event.key == pygame.K_RIGHT:
-                    currentBlock.x += 1
+                    currentBlock.y += 1
                     if checkConflict(grid, currentBlock, [Conflict.BOTTOM, Conflict.TOP]) != Conflict.NONE:
-                        currentBlock.x -= 1
+                        currentBlock.y -= 1
                 elif event.key == pygame.K_UP:
                     currentBlock.rotation = (currentBlock.rotation + 1) % len(currentBlock.block)
                     if checkConflict(grid, currentBlock,[Conflict.TOP]) != Conflict.NONE:
@@ -171,6 +194,7 @@ def gameStart(surface):
         if droppedBlock:
             currentBlock = getRandomBlock()
             grid = copy.deepcopy(copiedGrid)
+            deleteLine(grid)
 
         updateScreen(surface, copiedGrid)
         copiedGrid = copy.deepcopy(grid)
